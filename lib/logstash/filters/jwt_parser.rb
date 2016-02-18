156132f11b468f792df6d27b17d279fcec54a7bc
Greg <gabdo@u.washington.edu>
2016-02-18 08:58:50 +0000

    Restarted

--- lib/logstash/filters/jwt_parser.rb
+++ lib/logstash/filters/jwt_parser.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require "logstash/filters/base"
 require "logstash/namespace"
+require "jwt"
 
 # This example filter will replace the contents of the default 
 # message field with whatever you specify in the configuration.
@@ -17,10 +18,10 @@ class LogStash::Filters::Example < LogStash::Filters::Base
   #   }
   # }
   #
-  config_name "example"
+  config_name "jwt_parser"
   
   # Replace the message with this value.
-  config :message, :validate => :string, :default => "Hello World!"
+  config :token, :validate => :string
   
 
   public
@@ -30,12 +31,17 @@ class LogStash::Filters::Example < LogStash::Filters::Base
 
   public
   def filter(event)
+    @logger.warn("jwt_parser filter: reveived event", :type => event["type"])
+    
 
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
-      event["message"] = @message
-    end
+#    payload = {:data => 'test'}
+    
+    # IMPORTANT: set nil as password parameter
+#    token = JWT.encode payload, nil, 'none'
+
+    decoded_token = JWT.decode token, nil, false
+    event['token'] = decoded_token
+#    puts decoded_token
 
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
--- logstash-filter-jwt.gemspec
+++ logstash-filter-jwt.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
-  s.name = 'logstash-filter-example'
-  s.version         = '2.0.3'
+  s.name = 'logstash-filter-jwt'
+  s.version         = '1.0'
   s.licenses = ['Apache License (2.0)']
   s.summary = "This example filter replaces the contents of the message field with the specified value."
   s.description = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
@@ -19,5 +19,6 @@ Gem::Specification.new do |s|
 
   # Gem dependencies
   s.add_runtime_dependency "logstash-core", ">= 2.0.0", "< 3.0.0"
+  s.add_runtime_dependency "jwt", "1.5.2"
   s.add_development_dependency 'logstash-devutils'
 end
